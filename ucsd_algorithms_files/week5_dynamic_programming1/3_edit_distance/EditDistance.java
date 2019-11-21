5392e2c913f8135ded996f64d8d58dead25fc272
Kevin Jackson <kj31428@gmail.com>
2019-11-21 13:17:20 -0800

    Adds Knapsack discreet item algorithm.

--- ucsd_algorithms_files/week5_dynamic_programming1/3_edit_distance/EditDistance.java
+++ ucsd_algorithms_files/week5_dynamic_programming1/3_edit_distance/EditDistance.java
@@ -1,17 +1,89 @@
 import java.util.*;
 
 class EditDistance {
-  public static int EditDistance(String s, String t) {
-    //write your code here
-    return 0;
-  }
-  public static void main(String args[]) {
-    Scanner scan = new Scanner(System.in);
+    public static int editDistance(String a, String b) {
+        // Make a table
+        int[][] matrix = new int[a.length() + 1][b.length() + 1];
 
-    String s = scan.next();
-    String t = scan.next();
+        // Fill it out
+        for (int ai = 0; ai <= a.length(); ai += 1)  {
+            for (int bi = 0; bi <= b.length(); bi += 1) {
 
-    System.out.println(EditDistance(s, t));
-  }
+                // Start with filled out edges
+                if (ai == 0 && bi == 0) {
+                    matrix[ai][bi] = 0;
+                    continue;
+                }
+                if (ai == 0) {
+                    matrix[ai][bi] = matrix[ai][bi - 1] + 1;
+                    continue;
+                }
+                if (bi == 0) {
+                    matrix[ai][bi] = matrix[ai -1][bi] + 1;
+                    continue;
+                }
 
+                // Now we'll fill out the real meat of the table
+                // If match return upper-left
+                if (a.charAt(ai - 1) == b.charAt(bi - 1)) {
+                    matrix[ai][bi] = matrix[ai - 1][bi - 1];
+                }
+
+                // If mismatch take the min3 of the left, upper-left, and upper + 1
+                else {
+                    int left = matrix[ai][bi - 1] + 1;
+                    int upperLeft = matrix[ai - 1][bi - 1] + 1;
+                    int upper = matrix[ai - 1][bi] + 1;
+                    matrix[ai][bi] = min3(left, upperLeft, upper);
+                }
+            }
+        }
+
+        // Debug
+        // printMatrix(matrix);
+
+        // Answer is the bottom right
+        return matrix[a.length()][b.length()];
+    }
+
+    private static void printMatrix(int[][] m) {
+        for (int col = 0; col < m.length; col += 1) {
+            for (int row = 0; row < m[0].length; row += 1) {
+                System.out.print(m[col][row]);
+            }
+            System.out.println();
+        }
+    }
+
+    // Take the minimum of 3 ints
+    private static int min3(int x, int y, int z) {
+        if (x <= y && x <= z)
+            return x;
+        if (y <= x && y <= z)
+            return y;
+        else
+            return z;
+    }
+
+    public static void main(String args[]) {
+
+        boolean TESTING = false;
+
+        if (TESTING) {
+            System.out.println("ab,ab: " + (editDistance("ab", "ab") == 0 ? "Pass" : "Fail"));
+            System.out.println("short,ports: " + (editDistance("short", "ports") == 3 ? "Pass" : "Fail"));
+            System.out.println("editing, distance: " + (editDistance("editing", "distance") == 5 ? "Pass" : "Fail"));
+            System.out.println("cart, march: " + (editDistance("cart", "march") == 3 ? "Pass" : "Fail"));
+        }
+
+        else {
+            // Get input
+            Scanner scan = new Scanner(System.in);
+            String s = scan.next();
+            String t = scan.next();
+
+            // Output
+            System.out.println(editDistance(s, t));
+        }
+    }
 }
--- ucsd_algorithms_files/week6_dynamic_programming2/1_maximum_amount_of_gold/Knapsack.java
+++ ucsd_algorithms_files/week6_dynamic_programming2/1_maximum_amount_of_gold/Knapsack.java
@@ -1,18 +1,65 @@
 import java.util.*;
 
 public class Knapsack {
-    static int optimalWeight(int W, int[] w) {
-        //write you code here
-        int result = 0;
-        for (int i = 0; i < w.length; i++) {
-          if (result + w[i] <= W) {
-            result += w[i];
-          }
+    static int optimalWeight(int W, int[] items) {
+
+        // Dynamic bottom up solution
+        // Make a table for W + 1 and items + 1
+        int[][] tab = new int[items.length + 1][W + 1];
+
+        // Fill out table
+        for (int col = 0; col <= items.length; col += 1) {
+            for (int row = 0; row <= W; row += 1) {
+
+                // Initialize first row
+                if (row == 0) {
+                    tab[col][row] = 0;
+                }
+
+                // Initialize first col
+                else if (col == 0) {
+                    tab[col][row] = 0;
+                }
+
+                // The real work
+                else {
+                    // Fill out max of item above, or item above and left
+                    int above = tab[col - 1][row];
+                    int item  = items[col - 1];
+
+                    // Check if there's room for the item.
+                    if (item <= row) {
+                        int aboveLeft = tab[col - 1][row - item] + item;
+                        tab[col][row] = Math.max(above, aboveLeft);
+                    }
+                    // If not you have to use what's above.
+                    else {
+                        tab[col][row] = above;
+                    }
+                }
+            }
         }
-        return result;
+
+        return tab[items.length][W];
     }
 
     public static void main(String[] args) {
+
+        // Change below to test!
+        boolean TESTING = false;
+
+        if (TESTING) {
+            int result = optimalWeight(10, new int[] { 1, 4, 8 });
+            if (result == 9) {
+                System.out.println("Pass");
+            }
+            else {
+                System.out.println("Expected 9: got " + result);
+            }
+            return;
+        }
+
+        // Get input
         Scanner scanner = new Scanner(System.in);
         int W, n;
         W = scanner.nextInt();
@@ -21,7 +68,8 @@ public class Knapsack {
         for (int i = 0; i < n; i++) {
             w[i] = scanner.nextInt();
         }
+
+        // Output
         System.out.println(optimalWeight(W, w));
     }
 }
-
